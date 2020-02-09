0da88bbcbce9060f46bd5a950c3a0b2721ec4127
Castigere <even.haasted@gmail.com>
2020-02-09 15:16:23 +0100

    parameters

--- dist/index.js
+++ dist/index.js
@@ -491,18 +491,33 @@ module.exports = require("os");
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const slackToken = core.getInput('slack-token');
+  const picturePath = core.getInput('picture-path');
+  const channels = core.getInput('channels');
+
+  console.log('Cypress to slack test reporter');
+  console.log('Slack tokeb:', slackToken);
+  console.log('Picture path', picturePath);
+  console.log('Channels', channels);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
 
 /***/ }),
 
