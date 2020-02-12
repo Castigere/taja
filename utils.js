852f58f5b56841196d9984347d6f424d99e267eb
Even Haasted <even.haasted@nav.no>
2020-02-12 16:35:45 +0100

    test

--- utils.js
+++ utils.js
@@ -44,12 +44,11 @@ exports.postMessage = (message, channels, slackToken, threadTs) => {
         headers
     }).then(response => {
         console.log('RESPONSE', response.data)
-        return response.data.ts
-    })
-        .catch(err => {
-            console.log('ERROR', err);
-            return err
-        });
+        return response.data.ok ? response.data.ts : new Error(response.data.error)
+    }).catch(err => {
+        console.log('ERROR', err);
+        return err
+    });
 };
 
 exports.getFilenamesFromSubdirs = path => {
